e89374d910ba3282da69cdd38d2d340b70332f7b
JoshDW19 <joshdw19@pcbsd.org>
2016-12-19 15:20:52 -0500

    Made some more changes to the config and searchresults page.

--- trueos-handbook/conf.py
+++ trueos-handbook/conf.py
@@ -190,7 +190,7 @@ html_static_path = ['_static']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-html_extra_path = ['trueos-handbook/themes/trueos_style/searchresults.html']
+# html_extra_path = ['trueos-handbook/themes/trueos_style/searchresults.html']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
